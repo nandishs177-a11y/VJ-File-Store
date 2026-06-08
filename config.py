04a1705812d9c70eb0c28b8245c4018f5362d62b
nandishs177-a11y <nandishs177@gmail.com>
2026-06-08 16:45:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -49,8 +49,8 @@ AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Second
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1003800962005"))
 
 # File Caption Information
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
-BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
+BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", "")
 
 # Enable - True or Disable - False
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), False)
